50b78b20ac7fcc02cde61981834258c1ba443ffd
Aryna Reutska <arina.reutskaya110@gmail.com>
2023-11-14 21:38:56 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
 from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def validate(self, value):
+        if len(value) < 10 and ...:
+            raise ValueError('Phone number should be 10 symbols')
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+    def add_phone(self, phone_number):
+        phone = Phone(phone_number)
+        phone.validate(phone_number)
+        if phone not in self.phones:
+            self.phones.append(phone)
+
+    def find_phone(self, phone_number: str):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone_number
+
+    def edit_phone(self, old_phone, new_phone):
+        pass
+
+    def remove_phone(self, phone):
+        pass
+
+
+class AddressBook(UserDict):
+    def add_record(self, record: Record):
+        self.data[record.name.value] = record
+
+    def find(self):
+        pass
+
+    def delete(self):
+        pass
+
+
